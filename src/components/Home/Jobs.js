066dfeb000980f078a880dbdeca03a3587cd213b
Tengis Boldbaatar <tngis1413@gmail.com>
2022-08-25 14:12:30 +0800

    navbar max width responsive

--- src/components/Home/Jobs.js
+++ src/components/Home/Jobs.js
@@ -4,7 +4,7 @@ import Card from '../Card'
 function Jobs() {
   return (
     <div className='lg:pl-[19.5rem]'>
-      <div className='fixed py-8 max-w-3xl mx-auto w-full z-40 bg-[#0c0f13]'>
+      <div className='fixed py-8 lg:max-w-3xl mx-auto w-full z-40 bg-[#0c0f13]'>
         <h1 className='text-4xl font-semibold'>Jobs</h1>
       </div>
       <main className='pt-28 max-w-3xl mx-auto xl:max-w-none xl:ml-0 xl:mr-[15.5rem] xl:pr-16'>
