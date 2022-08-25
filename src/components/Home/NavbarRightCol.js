4af1508af4e87752ec2a6e0bb4b904de4c14da75
Tengis Boldbaatar <tngis1413@gmail.com>
2022-08-25 14:39:39 +0800

    home right navbar changed option to button

--- src/components/Home/NavbarRightCol.js
+++ src/components/Home/NavbarRightCol.js
@@ -6,12 +6,18 @@ function NavbarRightCol() {
   return (
     <div className='fixed z-40 top-[3.8125rem] bottom-0 right-[max(0px,calc(50%-45rem))] w-[19.5rem] pb-10 px-8 overflow-y-auto hidden xl:block'>
       <div className='sticky top-0 -ml-0.5 pointer-events-none py-10 bg-[#0c0f13]'>
-        <select className='w-full relative pointer-events-auto bg-[#58182d] p-2 m-0'>
+        {/* <select className='w-full relative pointer-events-auto bg-[#58182d] p-2 m-0'>
           <option value="">Last week</option>
           <option value="">Last month</option>
           <option value="">Last year</option>
           <option value="">All time</option>
-        </select>
+        </select> */}
+        <ul className='flex justify-around pointer-events-auto cursor-pointer underline-offset-2 uppercase font-semibold'>
+          <li className='hover:text-[#913151] duration-150'><a href="#"></a>1w</li>
+          <li className='hover:text-[#913151] duration-150'><a href="#"></a>1m</li>
+          <li className='hover:text-[#913151] duration-150'><a href="#"></a>1y</li>
+          <li className='hover:text-[#913151] duration-150'><a href="#"></a>All time</li>
+        </ul>
       </div>
       <ul className='relative -z-50 space-y-4'>
         <li className=''>
