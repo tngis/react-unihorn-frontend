39457cfdb149e1a3a01c4553161b161b1f7face5
Tengis Boldbaatar <tngis1413@gmail.com>
2022-08-25 10:22:26 +0800

    styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,29 @@
 import React from 'react'
-import Navbar from '../components/Navbar'
-import NavbarAdd from '../components/NavbarAdd'
-import NavbarCol from '../components/NavbarCol'
-import Jobs from '../components/Jobs'
+import Navbar from '../components/Home/Navbar'
+import NavbarAdd from '../components/Home/NavbarAdd'
+import NavbarLeftCol from '../components/Home/NavbarLeftCol'
+import NavbarRightCol from '../components/Home/NavbarRightCol'
+import Jobs from '../components/Home/Jobs'
+import Card from '../components/Card'
+import { Link } from 'react-router-dom'
 
 function Home() {
   return (
-    <div className='bg-gray-200'>
-      <Navbar />
+    <div className=' text-[#FBDCC4]'>
       <NavbarAdd />
-      <NavbarCol />
-      <Jobs />
-    </div>
+      <Navbar />
+      <div className='overflow-hidden'>
+        <div className='max-w-[1440px] mx-auto px-4 sm:px-6 md:px-8'>
+          <div className='border-pink-800 hidden lg:block fixed z-20 inset-0 top-[3.8125rem] left-[max(0px,calc(50%-45rem))] right-auto w-[19.5rem] pb-10 px-8 overflow-y-auto'>
+            <NavbarLeftCol />
+          </div>
+          <Jobs />
+          <NavbarRightCol />
+        </div>
+      </div >
+      <div className="fixed inset-0 -z-40 bg-[#0c0f13]"></div>
+    </div >
+
   )
 }
 
